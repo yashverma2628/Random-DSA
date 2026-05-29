82eb0e22edcb79c707476887cdd04d23394035d5
yashverma2628 <gourishankarverma2628@gmail.com>
2026-05-29 22:51:31 +0530

    print maximum sum  of an array

---
+++ arrays/maxsum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int maxsum(int arr[],int n){
+
+ int sum=0;
+    int maxisum=0;
+for (int i =0;i<n;i++){
+
+    sum = sum +arr[i];
+    if (maxisum<=sum) {maxisum =sum;}
+    if (sum <=0){sum =0;}
+
+    }
+return maxisum ;
+}
+
+int main () {
+
+     int arr[]={1,-2,3,4,-5,-6,7,8,-3,4,-1,9,4}; // maximum sum = 28
+    int n = sizeof(arr)/sizeof(int);
+   int x= maxsum( arr,n);
+   cout << "maxsum is :" << x << endl;
+    
+
+return 0;    
+}
