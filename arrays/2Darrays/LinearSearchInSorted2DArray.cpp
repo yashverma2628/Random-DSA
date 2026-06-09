e2e60f2d4d9c04104bc172c9e49529ed3ca63b5c
yashverma2628 <gourishankarverma2628@gmail.com>
2026-06-09 23:30:14 +0530

    finding the element in a 2d sorted array by brute force approach or linear search approach

---
+++ arrays/2Darrays/LinearSearchInSorted2DArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream> // finding the element in a 2d sorted array by brute force approach or linear search approach
+using namespace std;
+
+int matrix (int mat [][6],int n,int m, int key){
+
+    for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+            {
+        if (mat[i][j] == key) { cout << "key found at " << i << "," << j ; return 1;}
+            }
+
+    }cout << endl;
+    
+    return 0;
+}
+
+int main(){
+    int key = 8;
+ int mat[6][6]={ {1,2,3,4,5,6},
+                    {7,8,9,10,11,12},
+                    {13,14,15,16,17,18},
+                    {19,20,21,22,23,24},
+                    {25,26,27,28,29,30},
+                    {31,32,33,34,35,36} };
+    int n=6, // no. of rows
+        m=6; // no. of columns
+       if( !matrix( mat,n,m,key) ){ cout << "element not found";}
+    
+
+
+    return  0 ;
+}
